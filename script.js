ceeb0860dac8fead1d2c5f986b3b17861a4f5631
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-08 17:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'A Tajmahal'];
+
+let rev_article = (name) => name.replace( /^(a|an|the)\s+/i, "" );
+
+let finalSpots = touristSpots.map(rev_article).sort().map(val=> 
+     touristSpots.filter(str=> str.includes(val))
+
+)
+
+let ul = document.getElementById("bands");
+
+finalSpots.forEach(function(bandName) {
+  let li = document.createElement("li");
+  li.textContent = bandName;
+  ul.appendChild(li);
+});
+
+
+
+
 
